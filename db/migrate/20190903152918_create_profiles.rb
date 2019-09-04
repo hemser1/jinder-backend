c2570dc5b4fe7d7e754503204ccea37d2eb4a0ad
Josh Hemsley <josh@breathehr.com>
2019-09-04 16:17:10 +0100

    tweaked session controller to account for current user access

--- db/migrate/20190903152918_create_profiles.rb
+++ db/migrate/20190903152918_create_profiles.rb
@@ -3,7 +3,7 @@ class CreateProfiles < ActiveRecord::Migration[5.1]
     create_table :profiles do |t|
       t.string :first_name
       t.string :last_name
-      t.belongs_to :user
+      t.belongs_to :user, index: true
       t.string :industry
       t.string :skills
       t.timestamps
