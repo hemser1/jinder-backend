b6265ac99d7aac3442d55e3b8bbc3933b416caa1
Josh Hemsley <josh@breathehr.com>
2019-09-05 11:36:53 +0100

    added employers controller and model which creates employer and returns auth token in json

---
+++ app/controllers/employers_controller.rb
@@ -0,0 +1,28 @@
+class EmployersController < Devise::RegistrationsController
+
+  def create
+    @employer = Employer.new(employers_params)
+
+    if @employer.save!
+      p "LOOK HERE MOTEEEE"
+      render json: @employer.as_json(only: [:email, :authentication_token, :id]), status: :created
+    else
+      head(:unprocessable_entity)
+    end
+  end
+
+  def destroy
+    @employer = Employer.where(id: params[:id]).first
+    if @employer.destroy
+      head(:ok)
+    else
+      head(:unprocessable_entity)
+    end
+  end
+
+  private
+
+  def employers_params
+    params.require(:employer).permit(:first_name, :last_name, :email, :password, :bio, :company_url)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190903152918) do
+ActiveRecord::Schema.define(version: 20190905103517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "employers", force: :cascade do |t|
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "bio", default: "", null: false
+    t.string "company_url", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "authentication_token", limit: 30
+    t.index ["authentication_token"], name: "index_employers_on_authentication_token", unique: true
+    t.index ["email"], name: "index_employers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_employers_on_reset_password_token", unique: true
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string "first_name"
