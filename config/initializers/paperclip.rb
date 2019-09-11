ebc3753575cdcd9c66ec57e8ec1960660343e34d
Josh Hemsley <josh@breathehr.com>
2019-09-11 10:59:55 +0100

    adding end point change again

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
-Paperclip::Attachment.default_options[:url] = 's3-website.eu-west-2.amazonaws.com'
+Paperclip::Attachment.default_options[:url] = 'jinder-pictures.s3-website.eu-west-2.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 # Paperclip::Attachment.default_options[:s3_host_name] = 'http://jinder-pictures.s3-website.eu-west-2.amazonaws.com'
